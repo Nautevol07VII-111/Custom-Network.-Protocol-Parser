5b700791cd8fe5b118477bf72a34acbc80360c16
Nile Jackson <nilejack@udel.edu>
2025-07-30 22:02:48 -0400

    feat: added command processing header file

---
+++ src/include/command_processing.h
@@ -0,0 +1,8 @@
+#ifndef COMMAND_PROCESSING_H
+#define COMMAND_PROCESSING_H
+
+void process_command(const unsigned char *command, int length);
+void simulate_sensor_reading(float *value);
+
+
+#endif
