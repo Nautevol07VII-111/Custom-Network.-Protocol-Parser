569e32db7412266c519f150267852523911e3022
Nile Jackson <nilejack@udel.edu>
2025-08-16 15:32:23 -0400

    fix: added missing constants

--- src/include/protocol.h
+++ src/include/protocol.h
@@ -1,23 +1,31 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-// Define packet structure (like a network packet an Arduino might send)
+#include <stdint.h>
+
+// Binary packet structure
 typedef struct {
-    unsigned char start_byte; 
-    unsigned char length;     
-    unsigned char payload[32]; 
-    unsigned char checksum;   
-    unsigned char end_byte;   
+    unsigned char start_byte;
+    unsigned char length;
+    unsigned char payload[32];
+    unsigned char checksum;
+    unsigned char end_byte;
 } Packet;
 
-//Constants
+// Constants
 #define START_BYTE 0xAA
 #define END_BYTE 0xBB
 #define MAX_PAYLOAD 32
+#define MAX_BUFFER_SIZE 64
 
+// Text protocol functions
+int is_valid_text_protocol(const unsigned char *data, int length);
+int extract_command(const unsigned char *data, int length, char *command);
 
-//declaring functions
-void create_packet(Packet *packet, unsigned char length);
-int parse_packet(Packet *packet);
+// Binary packet functions
+void create_packet(Packet *packet, const char *command);
+int text_to_binary(const char *text_command, Packet *packet);
+void print_packet(const Packet *packet);
+unsigned char calculate_checksum(const Packet *packet);
 
 #endif
